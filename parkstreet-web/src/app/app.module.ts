546de5ff2687997a25c0984e2d197e733291a336
unknown <dominguez.arevalo@gmail.com>
2021-02-10 06:02:21 -0600

    Removing not needed dependencies for front-end

--- parkstreet-web/src/app/app.module.ts
+++ parkstreet-web/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AgGridModule } from 'ag-grid-angular';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +11,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     AgGridModule.withComponents([]),
     HttpClientModule
   ],
